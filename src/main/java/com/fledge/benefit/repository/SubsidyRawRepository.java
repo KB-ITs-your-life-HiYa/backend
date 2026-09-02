4b9c3f5a56c7b8da2eda1a9f7e4fa40aa03066b9
hayan <yanha14@naver.com>
2026-09-02 15:58:03 +0900

    feat: 지원금 Repository 인터페이스 추가

---
+++ src/main/java/com/fledge/benefit/repository/SubsidyRawRepository.java
@@ -0,0 +1,7 @@
+package com.fledge.benefit.repository;
+
+import com.fledge.benefit.domain.SubsidyRaw;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubsidyRawRepository extends JpaRepository<SubsidyRaw, Long> {
+}
---
+++ src/main/java/com/fledge/benefit/repository/SubsidyRepository.java
@@ -0,0 +1,7 @@
+package com.fledge.benefit.repository;
+
+import com.fledge.benefit.domain.Subsidy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubsidyRepository extends JpaRepository<Subsidy, Long> {
+}

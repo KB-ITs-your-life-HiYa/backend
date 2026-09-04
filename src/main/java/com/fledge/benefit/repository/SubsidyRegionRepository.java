edaa4661b4c5bcda835c9da940e0501f03b42061
Hayan Jo <59385635+yanh2@users.noreply.github.com>
2026-09-04 17:44:37 +0900

    feat: 지원금 매칭 조회 API  (#32)
    * feat: 지원금 매칭 조회 API 추가
    
    * fix: 잘못 파싱된 지원금 3건의 지역 코드 수정

--- src/main/java/com/fledge/benefit/repository/SubsidyRegionRepository.java
+++ src/main/java/com/fledge/benefit/repository/SubsidyRegionRepository.java
@@ -3,5 +3,8 @@ package com.fledge.benefit.repository;
 import com.fledge.benefit.domain.SubsidyRegion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubsidyRegionRepository extends JpaRepository<SubsidyRegion, Long> {
+    List<SubsidyRegion> findBySubsidy_Id(Long subsidyId);
 }
